2cc3408d923f8f2139d3dab3609920453133e504
vbuttazzo <vbuttazzo@yahoo.com>
2016-12-02 08:18:15 +0100

    added library

---
+++ saturn.js
@@ -0,0 +1,261 @@
+"use strict";
+
+var sj = {
+	loaded: false,
+	endpoint: null,
+	onClickCallback: null,
+	onSubmitCallback: null,
+	init: function(p) {
+		document.addEventListener('DOMContentLoaded', function() {
+			document.body.addEventListener("click", sj.onclick, false);
+			document.body.addEventListener("submit", sj.onsubmit, false);
+
+			sj.loaded = true;
+			for (var i in sj.components) {
+				sj.component(i, sj.components[i]);
+			}
+
+			var event = document.createEvent('Event');
+			event.initEvent('SjStarted', true, true);
+			document.dispatchEvent(event);
+		}, false);
+	},
+	toKebab: function(s) {
+		return s.replace(/[A-Z]/g, function(m) { return "-"+m.toLowerCase() })
+	},
+	toKamel: function(s) {
+		return s.replace(/-[a-z]/g, function(m) { return m.substr(1).toUpperCase() })
+	},
+	_renderSingle: function(el, k, v) {
+		if (!sj.loaded || !el) return;
+		var list = k == "_" ? [el] : el.querySelectorAll("[name="+k+"]");
+		if (!list.length) return console.error("Element not found", el, k, v)
+
+		if (typeof v == "string" || typeof v == "number") v = { _: v };
+		else if (typeof v == "boolean") v = { hidden: !v };
+
+		for (var i in v) {
+			for (j = 0; j < list.length; j++) {
+				var l = list[j];
+				if (i == "_") {
+					if (l.tagName == "INPUT" && (l.type == "radio" || l.type == "checkbox")) {
+						l.checked = (l.value == v[i]);
+					} else if (l.tagName == "INPUT") {
+						l.value = v[i];
+					} else if (l.hasAttribute('data-html')) {
+						l.innerHTML = v[i];
+					} else {
+						l.innerHTML = "";
+						l.appendChild(document.createTextNode(v[i]));
+					}
+				} else if (i == "style") {
+					for (var j in v[i]) l.style[j] = v[i][j];
+				} else if (v[i] === false) {
+					l.removeAttribute(i);
+				} else if (v[i] === true) {
+					l.setAttribute(i, "");
+				} else {
+					l.setAttribute((i == "className" ? "class" : i), v[i]);
+				}
+			}
+		}
+	},
+	hasClass: function(el, c) {
+		return (el.className.search(/\bc\b/) > -1);
+	},
+	removeClass: function(el, c) {
+		var re = new RegExp("\\b\s*"+c+"\\b");
+		el.className = el.className.replace(re, '').replace(/^\s+|\s$/g, '');
+	},
+	addClass: function(el, c) {
+		if (!sj.hasClass(el, c)) el.className += " "+c;
+	},
+	toggleClass: function(el, c) {
+		if (sj.hasClass(el, c)) {
+			sj.addClass(el, c);
+		} else {
+			sj.removeClass(el, c);
+		}
+	},
+
+	parseHash: function(hash) {
+		var q = {};
+
+		var n = hash.search(/#\//);
+		if (n > -1) hash = hash.substr(n+2);
+
+		var p = hash.split(/[&=]/);
+		for (var i = 0; i < p.length; i+=2) q[p[i]] = p[i+1];
+		return q;
+	},
+
+	componentClass: function(id, obj) {
+
+		this.oncreate = function() {}
+		this.onshow = function() {}
+		this.onpostshow = function() {}
+		this.onprehide = function() {}
+		this.onhide = function() {}
+		this.onclick = function(ev, target) {}
+		this.onaction = function(ev, target) {
+			sj.onActionCallback(ev, this.el, target);
+		}
+		this.onsubmit = function(ev, target) {
+			sj.onSubmitCallback(ev, this.el, target);
+		}
+		this.onrequest = function() {}
+		this.onresponse = function() {}
+		this.onerror = function(json) {
+			sj.onErrorCallback(json);
+		}
+
+		this.hide = function() {
+			this.onprehide();
+			this.el.hidden = true;
+			this.onhide();
+		}
+		this.show = function() {
+			this.onshow();
+			this.el.hidden = false;
+			this.onpostshow();
+		}
+		this.visible = function(b) {
+			if (b) this.show(); else this.hide();
+		}
+
+		this.hasClass = function(c) { return sj.hasClass(this.el, c) }
+		this.removeClass = function(c) { return sj.removeClass(this.el, c) }
+		this.addClass = function(c) { return sj.addClass(this.el, c) }
+		this.toggleClass = function(c) { return sj.toggleClass(this.el, c) }
+
+		this.render = function(p, sub) {
+			var el;
+			if (!p || typeof p != "object") return;
+			if (typeof sub == "string") {
+				el = this.el.querySelector("[name="+sub+"]");
+			} else if (typeof sub == "object") {
+				var t = sub;
+				while (t != document.body && t != this.el) t = t.parentNode;
+				el = (t == this.el) ? t : null;
+			}
+			if (!el) el = this.el;
+			for (var i in p) {
+				var l = el.querySelector("[name="+i+"]");
+				if (p[i] instanceof Array) {
+					while (!l.lastElementChild.hidden) l.removeChild(l.lastElementChild);
+					for (var j = 0; j < p[i].length; j++) {
+						var clone = l.firstElementChild.cloneNode(true);
+						clone.hidden = false;
+						for (var k in p[i][j]) {
+							sj._renderSingle(clone, k, p[i][j][k]);
+						}
+						l.appendChild(clone);
+					}
+				} else {
+					sj._renderSingle(el, i, p[i]);
+				}
+			}
+			if (this.onrender) this.onrender();
+		}
+
+		this.el = document.getElementById(id);
+		if (!this.el) throw "Component `"+id+"` not found";
+		this.el.setAttribute("data-component", "");
+		this.loop = this.el;
+
+		for (var i in obj) {
+			if (i == "loop") obj[i] = document.getElementById(obj[i]);
+			this[i] = obj[i];
+		}
+
+	},
+	components: {},
+	component: function(id, obj) {
+		sj.components[id] = (obj ? obj : {});
+		if (sj.loaded) {
+			sj.components[id] = new sj.componentClass(id, obj);
+			sj.components[id].oncreate();
+		}
+	},
+
+	c: function(id) {
+		return sj.components[id];
+	},
+
+	onclick: function(ev) {
+		var el = ev.target;
+		if (!el) return;
+		while (!el.hasAttribute("data-component") && el != document.body) el = el.parentNode;
+		if (el == document.body) return;
+		var id = el.id;
+		var target = ev.target;
+		while (target.tagName != "A" && target != el) target = target.parentNode;
+		if (target.tagName == "A") {
+			if (target.hash.search(/^#\//) > -1) {
+				sj.components[id].onrequest(ev, target);
+				sj.request(target.hash);
+				ev.preventDefault();
+			} else if (target.hash.search(/^#!/) > -1) {
+				sj.components[id].onaction(ev, target);
+			}
+		} else {
+			sj.components[id].onclick(ev, target);
+		}
+	},
+
+	onsubmit: function(ev) {
+		var el = ev.target;
+		while (!el.hasAttribute("data-component") && el != document.body) el = el.parentNode;
+		if (el == document.body) return;
+		var id = el.id;
+		var target = ev.target;
+		while (target.tagName != "FORM" && target != el) target = target.parentNode;
+		if (target.tagName == "FORM") {
+			if (target.action.search(/#\//) > -1) {
+				sj.components[id].onrequest(ev, target);
+				sj.request(target.action, target);
+				ev.preventDefault();
+			} else if (target.action.search(/#!/) > -1) {
+				sj.components[id].onsubmit(ev, el, target);
+			}
+		}
+	},
+
+	request: function(query, form, silent) {
+		if (!sj.endpoint) return console.error("No endpoint defined!");
+		var xhr = new XMLHttpRequest();
+		xhr.addEventListener("load", function(ev) {
+			var json = JSON.parse(ev.target.responseText);
+			if (json.error) {
+				sj.components[json.component].onerror(json);
+			} else if (json.component && sj.components[json.component]) {
+				sj.components[json.component].onresponse(json);
+			} else {
+				console.error("Component `"+json.component+"` not found");
+			}
+			sj.removeClass(document.body, "wait");
+		}, false);
+		xhr.addEventListener("error", function(ev) {
+			console.error(ev);
+		}, false);
+
+		if (typeof query == "string") query = sj.parseHash(query);
+		var q = [];
+		if (A.user.is_logged) q.push('token='+A.user.token); // FIXME
+		for (var i in query) q.push(i+"="+encodeURIComponent(query[i]));
+		var url = sj.endpoint+"?"+q.join("&");
+
+		if (form) {
+			if (!(form instanceof FormData)) form = new FormData(form);
+			xhr.open('POST', url);
+			xhr.send(form);
+		} else {
+			xhr.open('GET', url);
+			xhr.send();
+		}
+
+		if (!silent) sj.addClass(document.body, "wait");
+	}
+};
+
+sj.init();
